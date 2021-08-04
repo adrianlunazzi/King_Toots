97993376981cd5c6632f3887f7d6dc65e5df2a51
adrianlunazzi <adrianlunazzi@mail.com>
2021-08-04 10:05:12 -0300

    mejoro funcionalidad del componente ItemCount. Agrego funcion onAdd

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,51 +1,123 @@
 import React, { useState } from "react";
 import "./itemcount.css";
 
-const useCounter = (initialValue = 0) => {
-  const [count, setCount] = useState(initialValue);
+const useCounter = (initialStock, initialValue = 1) => {
+  const [count, setCount] = useState((initialValue = 1));
+  const [stock, setStock] = useState((initialStock = 10));
 
   const increment = () => {
-    setCount(count + 1);
+    if (stock > 0) {
+      setCount(count + 1);
+    }
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    if (count > 1) {
+      setCount(count - 1);
+    }
   };
-  if (count < 0)
-    return {
-      count,
-      increment,
-    };
-  else {
-    return {
-      count,
-      increment,
-      decrement,
-    };
-  }
+
+  const handlerOnAdd = () => {
+    if (stock >= count) {
+      setStock(stock - count);
+    } else {
+      alert("No hay suficiente stock para realizar el pedido");
+    }
+  };
+
+  return {
+    count,
+    stock,
+    increment,
+    decrement,
+    handlerOnAdd,
+  };
 };
 
-export default function ItemCount() {
-  const { count, increment, decrement } = useCounter();
+export default function ItemCount(initialStock) {
+  const { stock, count, increment, decrement, handlerOnAdd } = useCounter(1);
 
   return (
     <>
-      <div className="container-itemCount">
-        <div className="container-buttons">
-          <button className="btn-count" onClick={() => decrement()}>
-            <i class="far fa-caret-square-down"></i>
-          </button>
-          <div className="quantity">{count}</div>
-          <button className="btn-count" onClick={() => increment()}>
-            <i class="far fa-caret-square-up"></i>
-          </button>
+      {stock > 0 && count === 1 && (
+        <div className="container-itemCount">
+          <h6 className="stock">Stock Disponible {stock} </h6>
+
+          <div className="container-buttons">
+            <button className="btn-count" onClick={() => decrement()}>
+              <i className="far fa-caret-square-down"></i>
+            </button>
+            <div className="quantity">{count}</div>
+            <button className="btn-count" onClick={() => increment()}>
+              <i className="far fa-caret-square-up"></i>
+            </button>
+          </div>
+          <div className="container-buttons">
+            <button
+              className="btn-agregar-producto"
+              onClick={() => handlerOnAdd()}>
+              Agregar {count} Producto al carrito
+            </button>
+          </div>
         </div>
-        <div className="container-buttons">
-          <button className="btn-agregar-producto">
-            Agregar {count} Productos al carrito
-          </button>
+      )}
+      {stock > 0 && count > 1 && (
+        <div className="container-itemCount">
+          <h6 className="stock">Stock Disponible {stock} </h6>
+
+          <div className="container-buttons">
+            <button className="btn-count" onClick={() => decrement()}>
+              <i className="far fa-caret-square-down"></i>
+            </button>
+            <div className="quantity">{count}</div>
+            <button className="btn-count" onClick={() => increment()}>
+              <i className="far fa-caret-square-up"></i>
+            </button>
+          </div>
+          <div className="container-buttons">
+            <button
+              className="btn-agregar-producto"
+              onClick={() => handlerOnAdd()}>
+              Agregar {count} Productos al carrito
+            </button>
+          </div>
         </div>
-      </div>
+      )}
+
+      {stock === 0 && (
+        <div className="container-itemCount">
+          <h6 className="stock">Stock Disponible {stock} </h6>
+          <div className="container-buttons">
+            <button className="btn-count" onClick={() => decrement()}>
+              <i className="far fa-caret-square-down"></i>
+            </button>
+            <div className="quantity">{count}</div>
+            <button className="btn-count" onClick={() => increment()}>
+              <i className="far fa-caret-square-up"></i>
+            </button>
+          </div>
+          <div className="container-buttons">
+            <button className="btn-agregar-producto">No hay stock</button>
+          </div>
+        </div>
+      )}
+      {stock < 0 && (
+        <div className="container-itemCount">
+          <h6 className="stock">Stock Disponible {stock + 1} </h6>
+          <div className="container-buttons">
+            <button className="btn-count" onClick={() => decrement()}>
+              <i className="far fa-caret-square-down"></i>
+            </button>
+            <div className="quantity">{count}</div>
+            <button className="btn-count" onClick={() => increment()}>
+              <i className="far fa-caret-square-up"></i>
+            </button>
+          </div>
+          <div className="container-buttons">
+            <button className="btn-agregar-producto">No hay stock</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
